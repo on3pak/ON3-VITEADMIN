959e9d2d2581bc3643b7f8fbaf4090f7a6cbc0e8
on3pak <on3pak@on3pak.dev>
2026-05-09 12:32:42 +0200

    Remove location type and conditionally use viteSingleFile in production only

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,5 +4,4 @@ export * from './test';
 export * from './view';
 export * from './employee';
 export * from './city';
-export * from './location';
 export * from './workCenter';
--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,11 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), viteSingleFile()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    ...(process.env.NODE_ENV === 'production' ? [viteSingleFile()] : [])
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
